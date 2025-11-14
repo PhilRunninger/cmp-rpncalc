4f78d99334e3327bfae02d9eda5daa7105b2ba0e
Phil Runninger <philrunninger@gmail.com>
2025-11-13 22:13:22 -0500

    Break a line of code for better clarity. Replace vim.fn.join().

--- plugin/cmp_rpncalc.lua
+++ plugin/cmp_rpncalc.lua
@@ -2,7 +2,9 @@ vim.api.nvim_create_user_command(
     'RPN',
 
     function(opts)
-        local expression = opts.args ~= '' and opts.args or vim.fn.join(vim.api.nvim_buf_get_lines(0, opts.line1-1, opts.line2, false), ' ')
+        local expression = opts.args ~= ''
+            and opts.args
+            or table.concat(vim.api.nvim_buf_get_lines(0, opts.line1-1, opts.line2, false), ' ')
         local request = { context = { cursor_before_line = expression, cursor = { row = 1, col = 1 } } }
         require('cmp_rpncalc').complete(0, request,
             function(cmpResponse)
@@ -10,7 +12,6 @@ vim.api.nvim_create_user_command(
                     vim.api.nvim_echo({{'Error:', 'Error'}, {' RPN expression is invalid.', 'WarningMsg'}},false,{})
                     return
                 end
-
                 local response = {cmpResponse.items[opts.bang and 1 or 2].textEdit.newText}
                 if opts.args == '' then
                     vim.api.nvim_buf_set_lines(0, opts.line1-1, opts.line2, false, response)
@@ -27,5 +28,3 @@ vim.api.nvim_create_user_command(
         range = 0,
     }
 )
-
-
